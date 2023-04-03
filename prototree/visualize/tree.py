f678f143c07d7023d99a0b0973a71ad707d47fba
Clifford Wilmot <c.wilmot@appliedai.de>
2023-04-03 19:46:09 +0200

    Clean up tree leaves

--- prototree/visualize/tree.py
+++ prototree/visualize/tree.py
@@ -73,24 +73,25 @@ def _gen_pydot_nodes(
         node_vis_path: os.PathLike,
         class_names: tuple,
 ) -> list[pydot.Node]:
-    img = _gen_node_rgb(subtree_root, patches_path)
-    # TODO: Perhaps we should extract some pure functions here.
-    img_file = os.path.abspath(node_vis_path / f"node_{subtree_root.index}_vis.jpg")
-    img.save(img_file)
-
     if isinstance(subtree_root, InternalNode):
+        img = _gen_internal_node_img(subtree_root, patches_path)
+        # TODO: Perhaps we should extract some pure functions here.
+        img_file = os.path.abspath(node_vis_path / f"node_{subtree_root.index}_vis.jpg")
+        img.save(img_file)
+
         pydot_node = pydot.Node(subtree_root.index, image=f'"{img_file}"', xlabel=f'"{subtree_root.index}"', fontsize=6,
                                 labelfontcolor="gray50", fontname=FONT, shape="box")
         l_descendants = _gen_pydot_nodes(subtree_root.left, patches_path, node_vis_path, class_names)
         r_descendants = _gen_pydot_nodes(subtree_root.right, patches_path, node_vis_path, class_names)
         return [pydot_node] + l_descendants + r_descendants
     if isinstance(subtree_root, Leaf):
-        leaf_probs = copy.deepcopy(torch.exp(subtree_root.logits()).detach().numpy())
+        leaf_probs = torch.exp(subtree_root.logits()).detach()
         max_prob_inds = np.argmax(leaf_probs, keepdims=True)
-        predicted_classes = ",".join([class_names[i] for i in max_prob_inds])
+        max_prob = leaf_probs[max_prob_inds[0]]
+        predicted_classes = f"p = {max_prob:.5f}:\n" + ",\n".join([class_names[i] for i in max_prob_inds])
 
-        pydot_node = pydot.Node(subtree_root.index, image=f'"{img_file}"', label=predicted_classes, imagepos="tc",
-                                imagescale="height", labelloc="b", fontsize=10, penwidth=0, fontname=FONT)
+        pydot_node = pydot.Node(subtree_root.index, label=predicted_classes, labelfontcolor="gray50", fontname=FONT,
+                                shape="box")
         return [pydot_node]
 
     raise ValueError(f"Unknown node {subtree_root}.")
@@ -107,48 +108,6 @@ def _gen_pydot_edges(subtree_root: Node) -> list[pydot.Edge]:
         return []
 
     raise ValueError(f"Unknown node {subtree_root}.")
-
-
-def _gen_node_rgb(node: Node, patches_path: os.PathLike) -> Image:
-    if isinstance(node, Leaf):
-        img = _gen_leaf_img(node)
-    elif isinstance(node, InternalNode):
-        img = _gen_internal_node_img(node, patches_path)
-    else:
-        raise ValueError(f"Unknown node {node}.")
-
-    return img.convert("RGB")
-
-
-def _gen_leaf_img(node: Leaf) -> Image:
-    pixel_depth = 255
-    height = 24
-    footer_height = 10
-
-    node_probs = node.probs().detach().numpy()
-    distribution = (np.ones(node_probs.shape) - node_probs) * pixel_depth
-    num_classes = len(distribution)
-
-    width = min(36, num_classes)
-    # Correcting potential off-by-one errors.
-    scaler = math.ceil(width / num_classes)
-    width = scaler * num_classes
-
-    img = Image.new("F", (width, height))
-    # TODO: using img.load is discouraged, improve
-    pixels = img.load()
-
-    # TODO: Vectorize if this turns out to be a performance bottleneck.
-    for i in range(width):
-        for j in range(height - footer_height):
-            pixels[i, j] = distribution[int(i / scaler)]
-        # separate footer by black line
-        pixels[i, height - footer_height] = 0
-        for j in range(height - footer_height - 1, height):
-            # Set the bottom part of the node white so that the class label is readable.
-            pixels[i, j] = pixel_depth
-
-    return ImageOps.contain(img, (SINGLE_NODE_IMG_SIZE[0], height))
 
 
 def _gen_internal_node_img(node: InternalNode, patches_path: os.PathLike) -> Image:
@@ -171,4 +130,4 @@ def _gen_internal_node_img(node: InternalNode, patches_path: os.PathLike) -> Ima
     together.paste(patch_img, (0, 0))
     together.paste(bb_img, (w + INTERNAL_NODE_IMG_GAP, 0))
 
-    return together
+    return together.convert("RGB")
