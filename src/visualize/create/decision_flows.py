44a888366f678a634b3a07f8af36b7a03bda4a08
Clifford Wilmot <c.wilmot@appliedai.de>
2023-04-11 18:38:34 +0200

    Extract standard img

--- src/visualize/create/decision_flows.py
+++ src/visualize/create/decision_flows.py
@@ -77,31 +77,15 @@ def _decision_flow_dag(
             f"proto_subgraph_{proto_node.depth}", rank="same"
         )
 
-        proto_pydot_node = pydot.Node(
-            _node_name(proto_node),
-            image=f'"{proto_file}"',
-            imagescale=True,
-            fixedsize=True,
-            height=1.5,
-            width=1.5,
-            label="",
-            shape="plaintext",
-        )
+        proto_pydot_node = _img_pydot_node(_node_name(proto_node), proto_file, 1.5)
         proto_subgraph.add_node(proto_pydot_node)
 
         similarity = ancestor_similarity.highest_patch_similarity
         if went_right:
             bbox_file = decision_flow_dir / f"level_{proto_node.depth}_bounding_box.png"
             save_img(im_with_bbox, bbox_file)
-            bbox_pydot_node = pydot.Node(
-                _bbox_node_name(proto_node),
-                image=f'"{bbox_file}"',
-                imagescale=True,
-                fixedsize=True,
-                height=2,
-                width=2,
-                label="",
-                shape="plaintext",
+            bbox_pydot_node = _img_pydot_node(
+                _bbox_node_name(proto_node), bbox_file, 2.0
             )
 
             decision_edge = pydot.Edge(
@@ -166,16 +150,7 @@ def _original_im_components(
     original_file = decision_flow_dir / "original.png"
     save_img(original_im, original_file)
 
-    original_im_node = pydot.Node(
-        "original",
-        image=f'"{original_file}"',
-        imagescale=True,
-        fixedsize=True,
-        height=2,
-        width=2,
-        label="",
-        shape="plaintext",
-    )
+    original_im_node = _img_pydot_node("original", original_file, 2.0)
     original_label_node = pydot.Node(
         "original_label",
         label=f"Test image\n{true_class}",
@@ -184,7 +159,7 @@ def _original_im_components(
         shape="plaintext",
     )
     original_label_edge = pydot.Edge(
-        "original_label", "original", weight=100, style="invis"
+        "original_label", "original", weight=100, style="invis", minlen=0.5
     )
     original_to_proto_edge = pydot.Edge(
         "original",
@@ -195,6 +170,22 @@ def _original_im_components(
     original_edges = [original_label_edge, original_to_proto_edge]
 
     return original_nodes, original_edges
+
+
+def _img_pydot_node(node_name: str, im_file: os.PathLike, size: float) -> pydot.Node:
+    """
+    Creates a pydot node which resizes the im_file to the specified size..
+    """
+    return pydot.Node(
+        node_name,
+        image=f'"{im_file}"',
+        imagescale=True,
+        fixedsize=True,
+        height=size,
+        width=size,
+        label="",
+        shape="plaintext",
+    )
 
 
 def _bbox_node_name(node: Node) -> str:
