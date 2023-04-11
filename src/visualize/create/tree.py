0cdaf6e7beba498f6fbacc6e646ae0f0a0102026
Clifford Wilmot <c.wilmot@appliedai.de>
2023-04-11 23:06:29 +0200

    Start split

--- src/visualize/create/tree.py
+++ src/visualize/create/tree.py
@@ -1,3 +1,4 @@
+from functools import singledispatch
 import logging
 import os
 
@@ -69,40 +70,55 @@ def _pydot_tree(
     return graph_with_components(pydot_tree, pydot_nodes, [], pydot_edges)
 
 
+@singledispatch
 def _pydot_nodes(
     subtree_root: Node,
     patches_dir: os.PathLike,
     node_imgs_dir: os.PathLike,
     class_names: tuple,
 ) -> list[pydot.Node]:
-    match subtree_root:  # TODO: This function is quite big, should we use single dispatch instead?
-        case InternalNode():
-            img = _gen_internal_node_img(subtree_root, patches_dir)
-            # TODO: Perhaps we should extract some pure functions here.
-            img_file = node_imgs_dir / f"node_{subtree_root.index}_vis.jpg"
-            img.save(img_file)
+    raise ValueError(f"Unrecognized node {subtree_root}.")
 
-            pydot_node = pydot.Node(
-                _node_name(subtree_root),
-                image=f'"{img_file}"',
-                xlabel=f'"{subtree_root.index}"',
-                fontsize=6,
-                labelfontcolor="gray50",
-                fontname=FONT,
-                shape="box",
-            )
-            l_descendants = _pydot_nodes(
-                subtree_root.left, patches_dir, node_imgs_dir, class_names
-            )
-            r_descendants = _pydot_nodes(
-                subtree_root.right, patches_dir, node_imgs_dir, class_names
-            )
-            return [pydot_node] + l_descendants + r_descendants
-        case Leaf():
-            pydot_node = gen_leaf(subtree_root, class_names)
-            return [pydot_node]
-        case other:
-            raise ValueError(f"Unrecognized node {other}.")
+
+@_pydot_nodes.register
+def _pydot_nodes_internal(
+    subtree_root: InternalNode,
+    patches_dir: os.PathLike,
+    node_imgs_dir: os.PathLike,
+    class_names: tuple,
+) -> list[pydot.Node]:
+    img = _gen_internal_node_img(subtree_root, patches_dir)
+    # TODO: Perhaps we should extract some pure functions here.
+    img_file = node_imgs_dir / f"node_{subtree_root.index}_vis.jpg"
+    img.save(img_file)
+
+    pydot_node = pydot.Node(
+        _node_name(subtree_root),
+        image=f'"{img_file}"',
+        xlabel=f'"{subtree_root.index}"',
+        fontsize=6,
+        labelfontcolor="gray50",
+        fontname=FONT,
+        shape="box",
+    )
+    l_descendants = _pydot_nodes(
+        subtree_root.left, patches_dir, node_imgs_dir, class_names
+    )
+    r_descendants = _pydot_nodes(
+        subtree_root.right, patches_dir, node_imgs_dir, class_names
+    )
+    return [pydot_node] + l_descendants + r_descendants
+
+
+@_pydot_nodes.register
+def _pydot_nodes_leaf(
+    leaf: Leaf,
+    patches_dir: os.PathLike,
+    node_imgs_dir: os.PathLike,
+    class_names: tuple,
+) -> list[pydot.Node]:
+    pydot_node = gen_leaf(leaf, class_names)
+    return [pydot_node]
 
 
 def _pydot_edges(subtree_root: Node) -> list[pydot.Edge]:
