8e68eb26ffdb23963096ba85111a6fdcda2ddce0
Clifford Wilmot <c.wilmot@appliedai.de>
2023-04-11 11:23:51 +0200

    comments

--- src/visualize/prepare/explanations.py
+++ src/visualize/prepare/explanations.py
@@ -12,13 +12,16 @@ def data_explanations(
     loader: DataLoader,
     class_names: tuple,
 ) -> Iterator[tuple[LeafRationalization, int, tuple]]:
+    # TODO: This doesn't give a direct reference to the original image names/orderings (particularly if we shuffle the
+    #  data. We could try to plumb that through too, but we'd need to consider a possible loss in generality, e.g. if
+    #  the images don't come from files.
     """
     Produces an iterator of
-        leaf_explanation, true_label, class_names, explanation_counter
+        leaf_explanation, true_label, class_names
     An iterator is used to avoid OOMing on large datasets.
     """
     for x, y in loader:
         x, y = x.to(tree.device), y.to(tree.device)
         logits, node_to_probs, predicting_leaves, leaf_explanations = tree.explain(x)
         for leaf_explanation, true_label in zip(leaf_explanations, y):
-            yield leaf_explanation, true_label, class_names  # TODO: Might warrant a class.
+            yield leaf_explanation, true_label, class_names  # TODO: Might warrant a class, or is that overengineering?
