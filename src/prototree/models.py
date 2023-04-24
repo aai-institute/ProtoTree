0b9c28e18ed90e7b3e3fd668b84dfc335128d6cf
Clifford Wilmot <c.wilmot@appliedai.de>
2023-04-24 13:06:42 +0200

    minor

--- src/prototree/models.py
+++ src/prototree/models.py
@@ -570,7 +570,6 @@ class TreeSection(nn.Module):
         # This exponentially weighted moving average is designed to ensure stability of the leaf class probability
         # distributions (leaf.dist_params), by lowpass filtering out noise from minibatching in the optimization.
         leaf.dist_params.mul_(1.0 - self.leaf_opt_ewma_alpha)
-
         leaf.dist_params.add_(dist_update * self.leaf_opt_ewma_alpha)
 
     def log_leaves_properties(self):
