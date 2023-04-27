3fc277d70e23da8902351aa869825c3aebb3e144
Clifford Wilmot <c.wilmot@appliedai.de>
2023-04-27 13:15:10 +0200

    comment

--- src/proto/projection.py
+++ src/proto/projection.py
@@ -12,8 +12,8 @@ def project_prototypes(
     """
     Replaces each prototype with a given patch.
     Note: This mutates the prototype tensors.
-    TODO: We should probably not be mutating the tree (via the prototypes), as this is making the code less flexible and
-     harder to reason about.
+    TODO: We should probably not be mutating the tree (via the prototypes) after training, as this is making the code
+     less flexible and harder to reason about.
     """
     for internal_node, patch_info in node_to_patch_matches.items():
         node_proto_idx = tree.node_to_proto_idx[internal_node]
