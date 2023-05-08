431fde074d650065065e3362f7e6fb7c292f2830
Clifford Wilmot <c.wilmot@appliedai.de>
2023-05-08 14:46:31 +0200

    clean indexing

--- src/util/indexing.py
+++ src/util/indexing.py
@@ -1,17 +1,21 @@
 import torch
 
 
-def exclusion_range(idx: torch.Tensor, n: torch.Tensor):
-    r = torch.arange(n, device=idx.device)
-    return r[r != idx]
+def select_not(t: torch.Tensor, y: torch.Tensor):
+    # TODO: Vectorize this if it becomes a bottleneck (as of this commit it isn't).
+    """
+    Returns: A tensor where for each y_i in y, the tensor has a row with a flattened version of every row in t with
+    index not equal to y_i.
+    """
+    single_selections = [_select_not_unbatched(t, y_single) for y_single in y]
+    return torch.stack(single_selections, dim=0)
 
 
-def select_not_unbatched(t: torch.Tensor, y_single: torch.Tensor):
-    excl = exclusion_range(y_single, t.shape[0])
+def _select_not_unbatched(t: torch.Tensor, y_single: torch.Tensor):
+    excl = _exclusion_range(y_single, t.shape[0])
     return torch.flatten(t[excl, :])
 
 
-def select_not(t: torch.Tensor, y: torch.Tensor):
-    # TODO: Vectorize this if it becomes a bottleneck (as of this commit it isn't).
-    single_selections = [select_not_unbatched(t, y_single) for y_single in y]
-    return torch.stack(single_selections, dim=0)
+def _exclusion_range(idx: torch.Tensor, range_size: torch.Tensor):
+    r = torch.arange(range_size, device=idx.device)
+    return r[r != idx]
