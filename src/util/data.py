95fb33f328d4c277240641de1adc62212d613da6
Clifford Wilmot <c.wilmot@appliedai.de>
2023-05-17 22:34:51 +0200

    data changes

--- src/util/data.py
+++ src/util/data.py
@@ -2,7 +2,7 @@ import os
 
 import numpy as np
 import torchvision.transforms as transforms
-from matplotlib import pyplot, pyplot as plt
+from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 
@@ -36,20 +36,18 @@ def get_dataloaders(
         )
 
     train_loader = get_loader(train_set)
-    # make batch size smaller to prevent out of memory errors during projection
-    project_loader = get_loader(project_set, batch_size=batch_size // 4)
+    val_loader = get_loader(project_set)
     test_loader = get_loader(test_set)
-    return train_loader, project_loader, test_loader
+    return train_loader, val_loader, test_loader
 
 
 def get_data(
     augment_train_set=True, img_size=(224, 224)
 ) -> tuple[ImageFolder, ImageFolder, ImageFolder]:
     """
-
-    :param augment_train_set: only affects the train set, project set and test set are not augmented
+    :param augment_train_set: only affects the train set, val set and test set are not augmented
     :param img_size:
-    :return: tuple of type train_set, project_set, test_set, classes, shape
+    :return: tuple of type train_set, val_set, test_set, classes, shape
     """
     base_transform = get_base_transform(img_size)
 
@@ -69,9 +67,9 @@ def get_data(
     # TODO: relax hard-configured datasets, make this into a generic loader
     # TODO 2: we actually train on the corners, why? Is this to reveal biases?
     train_set = ImageFolder(train_dir, transform=train_transform)
-    project_set = ImageFolder(val_dir, transform=base_transform)
+    val_set = ImageFolder(val_dir, transform=base_transform)
     test_set = ImageFolder(test_dir, transform=base_transform)
-    return train_set, project_set, test_set
+    return train_set, val_set, test_set
 
 
 def save_img(img: np.ndarray, filepath: os.PathLike):
