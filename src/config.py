2419d2845654da496ee9b0cb0e5f7b5c611e345e
Clifford Wilmot <c.wilmot@appliedai.de>
2023-05-20 11:14:01 +0200

    config clean

--- src/config.py
+++ src/config.py
@@ -6,11 +6,6 @@ cub_images_dir = cub_dir / "images"
 
 dataset_dir = cub_dir / "dataset"
 
-# TODO: here cub is hardcoded, relax this
-# train_dir = dataset_dir / "train_corners"
-# project_dir = dataset_dir / "train_crop"
-# test_dir = dataset_dir / "test_full"
-
 train_dir = dataset_dir / "train_corners"
 val_dir = dataset_dir / "train_crop"
 test_dir = dataset_dir / "test_crop"
