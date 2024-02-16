6306db619ef65a4a285aca92673e301128ebff43
Michael Panchenko <m.panchenko@appliedai.de>
2024-02-16 11:43:42 +0100

    Reinstated deleted config.py

---
+++ src/config.py
@@ -0,0 +1,12 @@
+# TODO: remove duplication with config/config.yaml, move all config to python and remove the yaml based approach
+from pathlib import Path
+
+data_dir = Path("data")
+cub_dir = data_dir / "CUB_200_2011"
+cub_images_dir = cub_dir / "images"
+
+dataset_dir = cub_dir / "dataset"
+
+train_dir = dataset_dir / "train_corners"
+val_dir = dataset_dir / "train_crop"
+test_dir = dataset_dir / "test_crop"
